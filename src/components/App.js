b99b0ae6a7100d2e1129965e182685e0e69cefd5
Joe Johnson <joe@Joes-MacBook-Pro.local>
2018-03-06 11:00:38 -0800

    Async API call

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import ajax from '../ajax';
+import DealList from './DealList';
 
 class App extends React.Component {
+  state = {
+    deals: [],
+  }
+  async componentDidMount() {
+    const deals = await ajax.fetchInitialDeals();
+    // console.log(deals)
+    this.setState((prevState) => {
+      return { deals };
+    });
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>What up, chumps!?</Text>
+        {
+          this.state.deals.length > 0 ? (
+            <DealList deals={this.state.deals} />
+          ) : (
+            <Text style={styles.header}>What up, chumps!?</Text>
+          )
+        }
       </View>
     );
   }
